b4b49985d894a6f1aa5932ca1b7616f6fc52fbba
Hannah Quintos <hannah@quintos.com>
2024-01-28 15:08:57 -0500

    started gameover system

--- src/gameover-system.js
+++ src/gameover-system.js
@@ -1,5 +1,6 @@
 console.log("gameover-system.js : loaded");
 
+let stage = document.getElementById("stage");
 
 //global life counter
 let lifeTotal = 5;
@@ -8,5 +9,6 @@ let lifeTotal = 5;
 function gameOver() {
   if (lifeTotal === 0) {
     console.log("Game Over");
+    stage.style.display = "none";
   }
 }
